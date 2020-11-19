957492a516b35c50671d2858d5e24320004e656e
wermuthj <wermuthj@bzz.ch>
2020-11-19 14:54:43 +0100

    x

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const { doesNotMatch } = require("assert");
+
 var clicks = 0;
 function hello() {
   clicks += 1;
@@ -31,4 +33,32 @@ function ageTester() {
   }
 }
 
+
+
+function find() {
+  var infos = [ {last: 'Doe', age: 39, height: 165}, {last: 'Bella', age: 22, height: 193}, {last: 'Jowsey', age: 98, height: 148}]
+
+  var a = document.getElementById('name').selectedIndex;
+  var b = document.getElementsByClassName('option1')[a].value;
+
+  var c = document.getElementById('info').selectedIndex;
+  var d = document.getElementsByClassName('option2')[c].value;
+
+  if (b === 'John') {
+    var search = infos[0];
+  } else if (b === 'Linda') {
+    var search = infos[1];
+  } else if (b === 'Harry') {
+    var search = infos[2];
+  }
+
+  if (d === 'last') {
+    var e = "'s last name is " + search.last;
+  } else if (d === 'age') {
+    var e = " is " + search.age + " years old";
+  } else if (d === 'height') {
+    var e = " is " + search.height + "cm tall";
+  }
  
+  document.getElementById('found').innerHTML = b + e;
+}
